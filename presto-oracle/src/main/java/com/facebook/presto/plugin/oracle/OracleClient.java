818f0ee279c108d490b9593829ce2109753035d8
Anant Aneja <anant.aneja@ibm.com>
2025-05-30 13:16:31 +0530

    Oracle connector enhancements in WxD
    Cherry picks of
    https://github.ibm.com/lakehouse/presto/commit/f486279042b07713027cce862b485c67df227fa5
    https://github.ibm.com/lakehouse/presto/commit/765f73554b099ad050672599f689d2a1c157ecae
    https://github.ibm.com/lakehouse/presto/commit/402af930b7bfc3f01d0ae0c940bbf6417b9c2e40
    https://github.ibm.com/lakehouse/presto/commit/9a982557f9dbdb8d83e86cae3f15f156dd332c9d
    
    Alchemy-item: (ID = 808) Oracle connector enhancements in WxD commit 1/1 - d29e4ec2

--- presto-oracle/src/main/java/com/facebook/presto/plugin/oracle/OracleClient.java
+++ presto-oracle/src/main/java/com/facebook/presto/plugin/oracle/OracleClient.java
@@ -70,7 +70,7 @@ public class OracleClient
         extends BaseJdbcClient
 {
     private static final Logger LOG = Logger.get(OracleClient.class);
-    private static final int FETCH_SIZE = 1000;
+    private final int fetchSize;
 
     private final boolean synonymsEnabled;
     private final int numberDefaultScale;
@@ -87,6 +87,7 @@ public class OracleClient
         requireNonNull(oracleConfig, "oracle config is null");
         this.synonymsEnabled = oracleConfig.isSynonymsEnabled();
         this.numberDefaultScale = oracleConfig.getNumberDefaultScale();
+        this.fetchSize = config.getFetchSize();
     }
 
     private String[] getTableTypes()
@@ -103,11 +104,13 @@ public class OracleClient
     {
         DatabaseMetaData metadata = connection.getMetaData();
         String escape = metadata.getSearchStringEscape();
-        return metadata.getTables(
+        ResultSet resultSet = metadata.getTables(
                 connection.getCatalog(),
                 escapeNamePattern(schemaName, Optional.of(escape)).orElse(null),
                 escapeNamePattern(tableName, Optional.of(escape)).orElse(null),
                 getTableTypes());
+        resultSet.setFetchSize(fetchSize);
+        return resultSet;
     }
 
     @Override
@@ -115,7 +118,7 @@ public class OracleClient
             throws SQLException
     {
         PreparedStatement statement = connection.prepareStatement(sql);
-        statement.setFetchSize(FETCH_SIZE);
+        statement.setFetchSize(fetchSize);
         return statement;
     }
 
