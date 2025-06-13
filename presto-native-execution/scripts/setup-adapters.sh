a267ce76627e5fb731951f3e8cd4e7a4d10db159
Christian Zentgraf <czentgr@us.ibm.com>
2025-06-13 10:57:21 -0400

    [native] Enable CPR for build and fix Arrow build testing
    The IBM SaasAuthenticator requirtes CPR which is only built
    when testing is enabled. However, in pipelines
    testing is not enabled and the build fails as a result.
    
    For Arrow to be used or the Flight Connector the
    ARROW_TESTING library is required. This is enabled
    using the BENCHMARK option.
    
    Alchemy-item: (ID = 777) [native] Enable CPR for build without testing enabled commit 1/1 - 489684355dad368f169c0011794caae3d09c661b

--- presto-native-execution/scripts/setup-adapters.sh
+++ presto-native-execution/scripts/setup-adapters.sh
@@ -20,6 +20,9 @@ JWT_VERSION="v0.6.0"
 PROMETHEUS_VERSION="v1.2.4"
 
 SCRIPT_DIR=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")
+# Make a copy of SCRIPT_DIR because it is changed when sourcing
+# setup-common.sh. Check if we need a fix in OSS.
+SCRIPT_DIR_COPY=${SCRIPT_DIR}
 if [ -f "${SCRIPT_DIR}/setup-common.sh" ]; then
   source "${SCRIPT_DIR}/setup-common.sh"
 else
@@ -49,6 +52,7 @@ function install_arrow_flight {
   # Arrow Flight enabled. The Velox version of Arrow is used.
   # NOTE: benchmarks are on due to a compilation error with v15.0.0, once updated that can be removed
   # see https://github.com/apache/arrow/issues/41617
+  source "${SCRIPT_DIR_COPY}/../velox/scripts/setup-rhel.sh"
   EXTRA_ARROW_OPTIONS=" -DARROW_FLIGHT=ON -DARROW_BUILD_BENCHMARKS=ON -DgRPC_SOURCE=BUNDLED -DProtobuf_SOURCE=BUNDLED "
   install_arrow
 }
