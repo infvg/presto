c6e9ab2050920a9acc1ed7a347ec91ee8f536e81
Xin Zhang <desertsxin@gmail.com>
2025-11-13 14:31:41 +0000

    Support execution by operator for table functions
    Co-authored-by: kasiafi <30203062+kasiafi@users.noreply.github.com>
    Co-authored-by: mohsaka <135669458+mohsaka@users.noreply.github.com>
    
    Alchemy-item: (ID = 934) Add TVF Java Support commit 2/5 - 11cf2e03c84bcdd05903a53597e5e2bbbb4d5b45

--- presto-main-base/src/main/java/com/facebook/presto/execution/SqlQueryExecution.java
+++ presto-main-base/src/main/java/com/facebook/presto/execution/SqlQueryExecution.java
@@ -650,7 +650,7 @@ public class SqlQueryExecution
 
     private void createQueryScheduler(PlanRoot plan)
     {
-        CloseableSplitSourceProvider splitSourceProvider = new CloseableSplitSourceProvider(splitManager::getSplits);
+        CloseableSplitSourceProvider splitSourceProvider = new CloseableSplitSourceProvider(splitManager);
 
         // ensure split sources are closed
         stateMachine.addStateChangeListener(state -> {
